34aa792a5183c2b065674b04c6b5966379f17baa
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-02 06:21:53 +0000

    Added data types and conversion operations file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "ashadulla@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Dhaka"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "aa@aa.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "nsgnjngndn"
 
 console.log(accountId);
 
