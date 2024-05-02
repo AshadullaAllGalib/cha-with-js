a08227218e513bd76a1f08f98d93beeb5f5a65d5
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-02 07:21:34 +0000

    add comparison file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// ======basic comparison=====
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// ====Confusion comparision====
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict comparison check both value and data type
+
+console.log("2" === 2);
