270f2885a4caba442e5c44c39f4b758fc4855b9e
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-03 10:57:59 +0000

    add dateInJs file

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,32 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); //date is object
+
+// let myCreatedDate = new Date(2023, 0, 23)// 0 means jan output: Jan23 2023
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);// Gives Milisecond  1jan 1970 to now 
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); gives output in secon
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
