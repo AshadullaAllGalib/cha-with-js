c7422d1bfe2af33dab96e9eff55e3dac4749cac3
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-03 19:46:04 +0000

    added new objects for better understand

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,82 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "ashadulla",
+            lastname: "galib"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)// assign all obj element to target obj
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));//give all key of an obj
+// console.log(Object.values(tinderUser));//give all values of an obj
+// console.log(Object.entries(tinderUser));//give all the element of an obj as array
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));//Checking property
+
+
+const course = {
+    coursename: "javaScript",
+    price: "999",
+    courseInstructor: "ashadulla"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "ashadulla",
+//     "coursename": "javaScript",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
