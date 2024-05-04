06c1ff65848423faa4f2004a22c3c6db84b467d3
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-04 19:04:43 +0000

    added new file switch case

---
+++ 04_control_flow/swirch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"//if number simple use number Ex: const month = 3
+
+switch (month) {
+    case "jan": //1
+        console.log("January");
+        break;
+    case "feb"://2
+        console.log("feb");
+        break;
+    case "march"://3
+        console.log("march");
+        break;
+    case "april"://4
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
