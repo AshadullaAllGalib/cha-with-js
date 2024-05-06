9bd17b0b27a742ae082bad3ec240658e3ae05c80
Ashadulla <118709836+AshadullaAllGalib@users.noreply.github.com>
2024-05-06 05:14:04 +0000

    added new file for while do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,32 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// In this code loop execute all one by one
+let score1 = 0
+
+do {
+    console.log(`Score is ${score1}`);
+    score1++
+} while (score1 <= 10);
+
+
+// first work than check condition one execute than check the condition
+let score2 = 11
+
+do {
+    console.log(`Score is ${score2}`);
+    score2++
+} while (score2 <= 10);
